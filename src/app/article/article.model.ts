bfb33c5cfb8e49f019fc86c4337a1a55576df947
jackylong1992 <jklongnguyen@gmail.com>
2019-05-30 08:24:57 +0700

    - test class and interface

---
+++ src/app/article/article.model.ts
@@ -0,0 +1,18 @@
+// export class ArticleModel {
+//     constructor(
+//         public age: number,
+//         public name: string,
+//     ) {  }
+// }
+
+// export class ArticleModel {
+//     public age: number;
+//     public name: string;
+// }
+
+export interface IArticle {
+    age: number;
+    name: string;
+}
+
+/** INFO: interface and article can be used interchangably */
--- src/app/article/articlewrapper.component.ts
+++ src/app/article/articlewrapper.component.ts
@@ -1,15 +1,26 @@
 import { Component } from '@angular/core';
 import * as $ from 'jquery';
 import { Router } from '@angular/router';
+import { IArticle } from './article.model';
+// import { ArticleModel } from './article.model';
 @Component({
   templateUrl: './articlewrapper.component.html',
   styleUrls: ['./articlewrapper.component.scss']
 })
 export class ArticleWrapperComponent {
+  model: IArticle;
   constructor(private router: Router) {
 
   }
   ngOnInit(): void {
       
+  }
+
+  bindModel() {
+    let fakeModel = {
+      name: "long",
+      age: 24
+    }
+    this.model = fakeModel;
   }
 }
