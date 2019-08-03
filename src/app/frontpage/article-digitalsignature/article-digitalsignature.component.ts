252c5030f6bc0a877d41c394a25751f3530011e1
jackylong1992 <jklongnguyen@gmail.com>
2019-08-04 02:06:24 +0700

    fix styling

---
+++ src/app/frontpage/article-digitalsignature/article-digitalsignature.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'pm-article-digitalsignature',
+  templateUrl: './article-digitalsignature.component.html',
+  styleUrls: ['./article-digitalsignature.component.css']
+})
+export class ArticleDigitalsignatureComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
---
+++ src/app/frontpage/article-guide/article-guide.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'pm-article-guide',
+  templateUrl: './article-guide.component.html',
+  styleUrls: ['./article-guide.component.css']
+})
+export class ArticleGuideComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
