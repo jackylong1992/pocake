4b70f9155452f87b9c7b869241ea8ec9f7bfcd2d
jackylong1992 <jklongnguyen@gmail.com>
2019-08-04 09:28:11 +0700

    latest

--- src/app/frontpage/article-digitalsignature/article-digitalsignature.component.ts
+++ src/app/frontpage/article-digitalsignature/article-digitalsignature.component.ts
@@ -3,7 +3,7 @@ import { Component, OnInit } from '@angular/core';
 @Component({
   selector: 'pm-article-digitalsignature',
   templateUrl: './article-digitalsignature.component.html',
-  styleUrls: ['./article-digitalsignature.component.css']
+  styleUrls: ['./article-digitalsignature.component.scss']
 })
 export class ArticleDigitalsignatureComponent implements OnInit {
 
