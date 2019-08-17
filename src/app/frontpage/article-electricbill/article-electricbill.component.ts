1e70cc9f6d8f06cc71f8e67888f46941cec9ae22
jackylong1992 <jklongnguyen@gmail.com>
2019-08-17 14:31:52 +0700

    latest

--- src/app/frontpage/article-electricbill/article-electricbill.component.ts
+++ src/app/frontpage/article-electricbill/article-electricbill.component.ts
@@ -3,7 +3,7 @@ import { Component, OnInit } from '@angular/core';
 @Component({
   selector: 'pm-article-electricbill',
   templateUrl: './article-electricbill.component.html',
-  styleUrls: ['./article-electricbill.component.css']
+  styleUrls: ['./article-electricbill.component.scss']
 })
 export class ArticleElectricbillComponent implements OnInit {
 
